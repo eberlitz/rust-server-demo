2e56307ff7988eaa9d679b36f78417a2297f42a8
eberlitz <eberlitz@everest-erp.com>
2021-07-10 13:06:23 -0300

    Spawn a server per route

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,11 @@ pub use anyhow::anyhow;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Client, Method, Request, Response, Server, StatusCode};
 use std::collections::HashMap;
+use std::convert::Infallible;
 use std::net::SocketAddr;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use tokio::sync::oneshot;
+use tokio::sync::Mutex; // TODO: Should avoid holding a lock before await, although I want singleflight
 
 type GenericError = Box<dyn std::error::Error + Send + Sync>;
 type HttpClient = Client<hyper::client::HttpConnector>;
@@ -16,15 +19,10 @@ static NOTFOUND: &[u8] = b"Not Found";
 #[tokio::main]
 async fn main() {
     let in_addr: SocketAddr = "127.0.0.1:3000".parse().unwrap();
-    let target: SocketAddr = ([127, 0, 0, 1], 3001).into();
     let client = Client::new();
     let workers = WorkerManager::new();
 
-    let rp = std::sync::Arc::new(Svc {
-        target,
-        client,
-        workers,
-    });
+    let rp = std::sync::Arc::new(Svc { client, workers });
 
     let make_svc = make_service_fn(move |_conn| {
         let rp = rp.clone();
@@ -39,7 +37,6 @@ async fn main() {
     let server = Server::bind(&in_addr).serve(make_svc);
 
     println!("Listening on http://{}", in_addr);
-    println!("Proxying on http://{}", target);
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
@@ -47,7 +44,7 @@ async fn main() {
 }
 
 struct WorkerManager {
-    workers: Arc<Mutex<HashMap<String, bool>>>,
+    workers: Arc<Mutex<HashMap<String, SocketAddr>>>,
 }
 impl WorkerManager {
     pub fn new() -> Arc<Self> {
@@ -57,22 +54,46 @@ impl WorkerManager {
         Arc::new(m)
     }
 
-    pub async fn get(&self, key: &str) -> Result<bool, AnyError> {
-        let mut map = self.workers.lock().unwrap();
+    pub async fn get(&self, key: &str) -> Result<SocketAddr, AnyError> {
+        let mut map = self.workers.lock().await;
         // Return a clone if the value is a complex type.
         if let Some(&value) = map.get(key) {
             Ok(value)
         } else {
-            println!("Creating {}", key);
-            let value = true;
-            map.insert(key.into(), value);
-            Ok(value)
+            let add = self.spawn(key).await?;
+            println!("Got: {}", add);
+            map.insert(key.into(), add);
+            Ok(add)
         }
+    }
+
+    pub async fn spawn(&self, key: &str) -> Result<SocketAddr, AnyError> {
+        println!("Creating {}", key);
+        // Setup new thread
+        async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+            Ok(Response::new("Hello, World!".into()))
+        }
+        let (tx, rx) = oneshot::channel();
+        tokio::spawn(async {
+            let make_svc =
+                make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+            let in_addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
+            let server = Server::bind(&in_addr).serve(make_svc);
+
+            println!("Listening on http://{}", server.local_addr());
+
+            tx.send(server.local_addr().clone()).unwrap();
+            if let Err(e) = server.await {
+                eprintln!("server error: {}", e);
+            }
+        });
+
+        let value = rx.await?;
+        Ok(value.clone())
     }
 }
 
 struct Svc {
-    target: SocketAddr,
     client: HttpClient,
     workers: Workers,
 }
@@ -104,13 +125,10 @@ impl Svc {
     ) -> Result<Response<Body>, GenericError> {
         let route_name = req.uri().path().strip_prefix("/proxy/").unwrap_or("");
         println!("Receive {}", route_name);
-        let r = self.workers.get(route_name).await?;
-        if r {
-            println!("Found route")
-        }
+        let target = self.workers.get(route_name).await?;
         let uri_string = format!(
             "http://{}{}",
-            self.target,
+            target,
             req.uri()
                 .path_and_query()
                 .map(|x| x.as_str())
